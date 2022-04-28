b8169e3546b34129b321af6d30dc779a8f7747f8
iniakunhuda <iniakunhuda@gmail.com>
2022-04-28 10:25:41 +0700

    Initial Commit

---
+++ KalkulaShopping/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KalkulaShopping
+//
+//  Created by Miftahul Huda on 28/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ KalkulaShopping/KalkulaShoppingApp.swift
@@ -0,0 +1,17 @@
+//
+//  KalkulaShoppingApp.swift
+//  KalkulaShopping
+//
+//  Created by Miftahul Huda on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct KalkulaShoppingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
